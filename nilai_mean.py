43f0a767da896e11835fda425a1cb0850919551a
Pall <fuadasfal32@gmail.com>
2024-12-05 18:41:05 +0800

    nilai_mean.py

---
+++ nilai_mean.py
@@ -0,0 +1,9 @@
+x = int(input("masukkan nilai x :"))
+y = int(input("masukkan nilai y :"))
+
+rata_rata = x + y / 2
+
+if rata_rata > 70:
+        print("anda lulus")
+else:
+        print("anda tidak lulus")
